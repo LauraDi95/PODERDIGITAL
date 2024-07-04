bfdb61fc455f2d2f994208be8f655644e6e7de9b
LauraDi95 <dianavillafan23@gmail.com>
2024-07-04 13:51:53 -0600

    primeraprueba

---
+++ JS/saludar1.js
@@ -0,0 +1,8 @@
+// funcion declarada
+// si el parentesis esta vacio la funcion es de tipo void
+function saludar1 (persona1, persdsona2) {
+    console.log("Hola+persona1+ 'y' +persona2");
+    console; console.log(Bienvenida);
+    return 'Hola';
+}
+saludar1('Aby', 'Fer');
