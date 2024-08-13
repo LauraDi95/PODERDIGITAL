af1137413980f7dff256174e2f098488ae63681e
LauraDi95 <dianavillafan23@gmail.com>
2024-08-13 03:26:06 -0600

    indexhtmllisto

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
+// botón muestra oculta botón cajas de presentación
 function muestra_oculta(id){
     let div = document.getElementById(id);
     if (div.style.display == "none"){
-     div.style.display = "flex";
+       div.style.display = "flex";
     } 
     else {
         div.style.display = "none";
     }
 } 
+
+// elemntos abrir y cerrar menú hamburguesa
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+// evento y función para abrir
+abrir.addEventListener("click",() => {
+    nav.classList.add("visible");
+})
+
+// elemntos y función para cerrar
+cerrar.addEventListener("click",() => {
+    nav.classList.remove("visible");
+})
 
